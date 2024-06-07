6c426077878b0b2fb1c66e060a229ac20bfa7968
Yemmieeyc <adewole_aina@yahoo.com>
2024-06-06 19:28:39 -0700

    wip

--- frontend/state/quotesSlice.js
+++ frontend/state/quotesSlice.js
@@ -1,7 +1,9 @@
 // ✨ create your `quotesSlice` in this module
+import { createSlice } from "@reduxjs/toolkit"
+
 
 let id = 1
-const getNextId = () => id++
+export const getNextId = () => id++
 const initialState = {
   displayAllQuotes: true,
   highlightedQuote: null,
@@ -26,3 +28,39 @@ const initialState = {
     },
   ],
 }
+export const quotesSlice = createSlice({
+  name: 'quotes',
+  initialState,
+  reducers: {
+    toggleVisibility(state){
+      state.displayAllQuotes = !state.displayAllQuotes
+    },
+    deleteQuote(state, action){
+      state.quotes = state.quotes.filter(qt => qt !== action.payload)
+    },
+    editQuoteAuthenticity(state, action){
+      const quoteToEdit = state.quotes.find(qt => qt.id === action.payload)
+      quoteToEdit.apocryphal = !quoteToEdit.apocryphal
+    },
+    setHighlightedQuote(state, action){
+      if(state.highlightedQuote === action.payload){
+        state.highlightedQuote = null
+      }else {
+        state.highlightedQuote = action.payload
+      }
+    },
+    createQuote(state, action){
+      state.quotes.push(action.payload)
+    }
+  }
+})
+
+export const {
+  createQuote,
+  deleteQuote,
+  editQuoteAuthenticity,
+  highlightedQuote,
+  toggleVisibility,
+} = quotesSlice.actions
+
+export default quotesSlice.reducer
